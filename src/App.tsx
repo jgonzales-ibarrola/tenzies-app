9a1dfefe813137ef2830df06d46c4f6a91c3dcd3
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-02-13 12:43:44 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+	return (
+		<main className="container m-auto">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</main>
+	);
+};
+
+export default App;
